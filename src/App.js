e5c5ad1b0408fbe9afdf8893ba8a3903e3520883
syla0 <sylerexha@hotmail.com>
2022-05-05 21:13:24 +0200

    Produkt Crud
    Crudi I parë

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import AddProduct from './AddProduct';
+import ProductItem from './ProductItem';
+const products = [
+  {
+    name: 'Tulla-001',
+    price: '10'
+  },
+  {
+    name: 'Tulla-002',
+    price: '14'
+  },
+  {
+    name: 'Tulla-003',
+    price: '12,99'
+  }
 
+];
+
+localStorage.setItem('products', JSON.stringify(products));
+
+class App extends Component {
+  constructor(props) {
+  super(props);
+
+  this.state = {
+    products: JSON.parse(localStorage.getItem('products'))
+  };
+  this.onAdd = this.onAdd.bind(this);
+  this.onDelete = this.onDelete.bind(this);
+  this.onEditSubmit = this.onEditSubmit.bind(this);
+}
+  componentWillMount() {
+   const products = this.getProducts();
+
+   this.setState ({ products });
+  }
+  getProducts() {
+    return this.state.products;
+  }
+    onAdd(name, price) {
+      const products = this.getProducts();
+
+      products.push({
+        name,
+        price
+      });
+      this.setState({ products });
+    }
+    onDelete(name) {
+    const products = this.getProducts();
+
+    const filteredProducts = products.filter(product => {
+      return product.name !== name;
+    });
+      this.setState({ products: filteredProducts });
+    }
+    onEditSubmit(name, price, originalName) {
+     let products = this.getProducts();
+
+     products = products.map(product => {
+       if (product.name === originalName) {
+         product.name = name;
+         product.price = price;
+         }
+         return product;
+     });
+     this.setState ({ products });
+
+    }
+   render() {
+    return (
+      <div className="App">
+        <h1>Products Manager</h1>
+        <AddProduct
+           onAdd={this.onAdd}
+
+        />
+        {
+          this.state.products.map(product=> { 
+            return (
+              <ProductItem
+              key={product.name}
+              {...product}
+              onDelete={this.onDelete}
+              onEditSubmit={this.onEditSubmit}
+
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
+}
 export default App;
